99e8fb4815e432c0173b421992e90d95755f6907
TheOneWith-3j <jashjoshi57@gmail.com>
2026-09-17 20:53:12 -0400

    fix: improve dev preview stability

--- src/app/ServiceWorkerRegistration.tsx
+++ src/app/ServiceWorkerRegistration.tsx
@@ -4,7 +4,15 @@ import { useEffect } from "react";
 
 export default function ServiceWorkerRegistration() {
   useEffect(() => {
-    if ("serviceWorker" in navigator) {
+    if (!("serviceWorker" in navigator)) return;
+    if (process.env.NODE_ENV === "development") {
+      navigator.serviceWorker.getRegistrations().then((registrations) => {
+        registrations.forEach((registration) => void registration.unregister());
+      });
+      caches.keys().then((keys) => {
+        keys.forEach((key) => void caches.delete(key));
+      });
+    } else {
       navigator.serviceWorker
         .register("/service-worker.js")
         .catch(() => undefined);
